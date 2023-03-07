50d0b0a3d6c5823842a5a66e020d050429c77318
Damir <massalimov.d@gmail.com>
2023-03-07 12:44:13 +0600

    task#27 done

---
+++ task#27/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+void Data()
+{
+    Console.Write("Enter the number = ");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    int result = Calculating(a);
+    Console.WriteLine($"Result = {result}");
+}
+
+int Calculating(int a)
+{
+    int sum = 0;
+    while (a > 0)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    return sum;
+}
+
+Data();
